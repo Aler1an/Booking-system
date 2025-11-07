5ae7c31eda3f80d0bb6e93cd5b392aeb06a822de
Maksim Yatsenyuk <yaesenykmax356@gmail.com>
2025-11-07 19:45:23 +0200

    resolve problems

--- frontend/script.js
+++ frontend/script.js
@@ -33,22 +33,3 @@ async function addBooking(e) {
 document.getElementById("loadBookingsBtn").addEventListener("click", loadBookings);
 document.getElementById("bookingForm").addEventListener("submit", addBooking);
 
-async function addBooking() {
-  const name = document.getElementById('name').value.trim();
-  const date = document.getElementById('date').value;
-
-  if (!name || !date) {
-    alert('Будь ласка, заповніть усі поля!');
-    return;
-  }
-
-  await fetch(API_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name, date })
-  });
-
-  document.getElementById('name').value = '';
-  document.getElementById('date').value = '';
-  await loadBookings();
-}
